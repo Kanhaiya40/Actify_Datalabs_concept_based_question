b0f39e457daab0c803ac79e9e245a43b2e83439c
Kanhaiya Kumar <kanhaiyasisteccs2015@gmail.com>
2021-02-23 07:39:35 +0530

    3rd point and 4th point has been updated

--- src/java_question_3/NumExtract.java
+++ src/java_question_3/NumExtract.java
@@ -3,7 +3,9 @@ package java_question_3;
 import java.util.ArrayList;
 
 /**
- * Extract numbers and find the smallest number from the below string “asdsdgbdfgjdfgn6sbdfh3hhj5j1jh5j66asryuop8ng2nb7mbb8k;0++)O)”
+ * Extract numbers and find the smallest
+ * number from the below string
+ * “asdsdgbdfgjdfgn6sbdfh3hhj5j1jh5j66asryuop8ng2nb7mbb8k;0++)O)”
  */
 public class NumExtract {
 
@@ -12,21 +14,26 @@ public class NumExtract {
         ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
-                StringBuilder out = new StringBuilder();
-                while (Character.isDigit(input.charAt(i))) {
-                    out.append(input.charAt(i));
+                while (i != input.length() && Character.isDigit(input.charAt(i))) {
+                    numbers.add(Integer.parseInt(String.valueOf(input.charAt(i))));
                     i++;
                 }
-                numbers.add(Integer.parseInt(out.toString()));
                 i--;
             }
         }
-        int min = numbers.get(0);
-        for (int num : numbers) {
-            if (num < min) {
-                min = num;
+        numbers.sort(Integer::compareTo);
+        StringBuilder outputUpdated = new StringBuilder();
+        int countNumberOfZeros = 0;
+        for (int number : numbers) {
+            if (number != 0) {
+                outputUpdated.append(number);
+            } else {
+                countNumberOfZeros++;
             }
         }
-        System.out.println(min);
+        while (countNumberOfZeros-- > 0) {
+            outputUpdated.append("0");
+        }
+        System.out.println(outputUpdated);
     }
 }
--- src/java_questions_4/WordReverser.java
+++ src/java_questions_4/WordReverser.java
@@ -4,23 +4,45 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * Reverse words in a sentence without using for loop and string and stringbuilder functions
+ * Reverse words in a sentence without using
+ * for loop and string and StringBuilder functions
  */
 public class WordReverser {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        char[] toChar = input.toCharArray();
         Stack<String> words = new Stack<>();
-        String[] splitWord = input.split("\\s+");
-        for (String wor : splitWord) {
-            words.push(wor);
+        Stack<String> output = reverseString(toChar, words, 0);
+        StringBuilder originalOutput = new StringBuilder();
+        while (!output.isEmpty()) {
+            originalOutput.append(output.pop()).append(" ");
         }
-        StringBuilder output = new StringBuilder();
-        while (!words.isEmpty()) {
-            output.append(words.pop());
-            output.append(" ");
+        System.out.println(originalOutput.toString());
+    }
+
+    private static Stack<String> reverseString(char[] toChar, Stack<String> words, int index) {
+        StringBuilder word = new StringBuilder();
+        int startIndex = index;
+        int lastIndex = startIndex;
+        while (true) {
+            if (lastIndex == toChar.length) {
+                while (startIndex != lastIndex) {
+                    word.append(toChar[startIndex++]);
+                }
+                words.push(word.toString());
+                break;
+            } else if (toChar[lastIndex] == ' ') {
+                while (startIndex != lastIndex) {
+                    word.append(toChar[startIndex++]);
+                }
+                words.push(word.toString());
+                reverseString(toChar, words, startIndex + 1);
+                break;
+            } else
+                lastIndex++;
         }
-        System.out.println(output);
+        return words;
     }
 }
